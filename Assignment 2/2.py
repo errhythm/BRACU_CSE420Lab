4110c7ab1d8eb940b8cbab352389170bb3c8e84a
Ehsanur Rahman Rhythm <ehsanur.rahman.rhythm@g.bracu.ac.bd>
2022-07-03 22:27:26 +0600

    Using the email rules RFC 2822 to check the emails

--- Assignment 2/2.py
+++ Assignment 2/2.py
@@ -1,11 +1,42 @@
-n = int(input("Please input number of lines you want to enter.\n"))
+f = open("input.txt", "r")
+code = f.readlines()
+code = [x.strip() for x in code]
 
-arr = []
+n = int(code[0])
+print(n)
+code.pop(0)
 
-for i in range(n):
-    m = input()
-    arr.append(m)
+symbols = [".", ",", "-", "_", ";", ":", "!", "?", "=", "+", "*", "/", "(", ")", "{", "}", "[", "]", "#", "$", "%", "^", "&", "|"]
+
+def checkValidEmail(email):
+    if email.count("@") == 1 and "." in email:
+        if email[0].isdigit() or email[-1].isdigit():
+            return False
+        else:
+            split_email = email.split("@")
+            for i in symbols:
+                if split_email[0][0] == i:
+                    return False
+                if split_email[0][-1] == i:
+                    return False
+                for j in range(len(split_email[0]) - 1):
+                    if split_email[0][j] == i:
+                        if split_email[0][j] == split_email[0][j + 1]:
+                            return False
+            symbols.pop(0)
+            for i in symbols:
+                for j in range(len(split_email[0]) - 1):
+                    if split_email[0][j] == i:
+                        return False
+            return True
 
 
 
-print(arr)
+
+for i in code:
+    if checkValidEmail(i):
+        print(i, code.index(i)+1)
+    else:
+        print("Invalid")
+
+print(code)
