afa874a303bc289441e5b0e6c553faa6d2c01eeb
Ehsanur Rahman Rhythm <ehsanur.rahman.rhythm@g.bracu.ac.bd>
2022-08-08 10:33:07 +0600

    Update 4.py

--- Assignment 4/4.py
+++ Assignment 4/4.py
@@ -13,7 +13,7 @@ reg = r"(public|private|default)( static)? (Boolean|char|byte|short|int|long|flo
 for line in code:
     if re.match(reg, line) and not re.match(mainMethod, line):
         method.append(re.findall("(Boolean|char|byte|short|int|long|float|double|String|void) (\w+)\((.+)?\)", line)[0])
-        print(method)
+        
 print("Methods:")
 for m in method:
     print("{} ({}), return type: {}".format(m[1], m[2], m[0]))
